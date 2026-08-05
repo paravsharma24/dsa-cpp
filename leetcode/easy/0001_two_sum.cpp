188a23c02b138d4bee6a6337cea240b73448cf99
PSOrigins <parav.sharma.328@gmail.com>
2026-08-05 22:49:19 +0530

    Solve LeetCode #1 Two Sum (Brute Force)

--- leetcode/easy/0001_two_sum.cpp
+++ leetcode/easy/0001_two_sum.cpp
@@ -5,7 +5,14 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Your solution
+        for(int i = 0; i < size(nums); i++) {
+            for(int j = (i + 1); j < size(nums); j++) {
+                if(nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
     }
 };
 
