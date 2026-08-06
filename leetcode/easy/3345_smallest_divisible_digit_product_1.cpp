be56bdd1b3651f440c83853d03e2b1cee36b742b
PSOrigins <parav.sharma.328@gmail.com>
2026-08-06 18:40:35 +0530

    3345_smallest_divisible_digit_product

---
+++ leetcode/easy/3345_smallest_divisible_digit_product_1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n = 551;
+    int t = 3;
+
+while(true) {
+    
+    int temp = n;
+    int product = 1;
+    
+    while(temp > 0) {
+        int digit = temp % 10;
+        product *= digit;
+        temp /= 10;
+    }
+
+    if(product % t == 0) {
+        cout << n;
+        break;
+    }
+
+    else{
+        n++;
+    }
+
+}
+
+
+
+    // cout << (temp /= 10) << endl;
+    // cout << temp;
+
+    return 0;
+}
